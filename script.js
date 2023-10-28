68e2e45323078e8074f3753071c5c3e46c3b2e98
ardden <139798693+ardden@users.noreply.github.com>
2023-10-28 23:16:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const typedText =
+  "Liam, es gibt etwas, das ich schon eine Weile mit dir teilen möchte. Ich fühle, dass wir eine starke Verbindung aufgebaut haben, und ich kann nicht anders, als an unsere gemeinsame Zukunft zu denken. Ich weiß, es mag so aussehen, als würde ich die Dinge überstürzen, aber ich war noch nie sicherer bei irgendetwas in meinem Leben. Du machst mich unglaublich glücklich, und ich kann es kaum erwarten, dich an meiner Seite zu haben. Liam, würdest du mir die große Ehre erweisen, mein Partner zu werden und mit mir auf diese Reise zu gehen? Möchtest du mit mir ausgehen? -Arden.";
+const typedTextElement = document.getElementById("typed-text");
+
+let charIndex = 0;
+
+function typeText() {
+  typedTextElement.textContent = typedText.slice(0, charIndex);
+  charIndex++;
+
+  if (charIndex <= typedText.length) {
+    setTimeout(typeText, 50); // Adjust the typing speed here
+  }
+}
+
+typeText();
